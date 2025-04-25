72e3d6184daef51aea9d38fc92f03c73c34ed3f2
Muhammad Usman Nazir <usmannazir428@gmail.com>
2025-04-26 04:16:56 +0500

    minor changes

--- server.js
+++ server.js
@@ -10,22 +10,26 @@ const httpServer = http.createServer(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "250mb" }));
+// app.use('/webhook/sendgrid', express.json({ type: 'application/json' }));
 
 app.get('/', (req, res) => res.json({ message: `Welcome to the Webhook Test Project` }));
 
 const data = [];
 
 app.get('/items', (req, res) => {
+    console.log(' Webhook Received:', req.body);
     res.json({ items: data });
 });
 
 // POST endpoint to add a new item
 app.post('/items', (req, res) => {
     const { name } = req.body;
+    console.log(' Webhook Received:', req.body);
     if (!name) {
         return res.status(400).json({ error: "Name is required" });
     }
     const newItem = { id: data.length + 1, name };
+    console.log(' Item Added:', newItem);
     data.push(newItem);
     res.status(201).json({ message: "Item added", item: newItem });
 });
